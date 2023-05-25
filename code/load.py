5a0a481a19ad0279e8294f744bd8bb1351d57fe4
Aiza Maksutova <71812108+aizamaksutova@users.noreply.github.com>
2023-05-25 12:58:41 +0300

    Update load.py

--- code/load.py
+++ code/load.py
@@ -7,14 +7,14 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 tensor_size = 10000000  # Adjust the size as per your requirements
 
 # Create large tensors on CPU and transfer them to GPU
-tensor1 = torch.randn(tensor_size).to(device)
-tensor2 = torch.randn(tensor_size).to(device)
+tensor1 = torch.randn(tensor_size).to('cpu')
+tensor2 = torch.randn(tensor_size).to('cpu')
 
 # Perform frequent data transfers between CPU and GPU
 for i in range(5000):  # Adjust the number of iterations as per your requirements
-    tensor1 = tensor1.to('cpu')
-    tensor2 = tensor2.to(device)
     tensor1 = tensor1.to(device)
+    tensor2 = tensor2.to(device)
+    tensor1 = tensor1.to('cpu')
     tensor2 = tensor2.to('cpu')
 
 print("Data transfers completed.")
